ed819329c5b7165fa9d06e4311043199c84be313
Hannah-Doerpholz <108814050+Hannah-Doerpholz@users.noreply.github.com>
2023-10-31 10:59:32 +0100

    Add external mapping file for the run script

---
+++ 30/converter/mapping_dict.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  2 08:44:52 2023
+
+@author: hannah
+"""
+
+idf_to_isa_investigation = {
+    "Term Source Name":"Term Source Name",
+    "Term Source File":"Term Source File",
+    "Term Source Version":"Term Source Version",
+    "Term Source Description":"Term Source Description", # this field is added through get_ontology_description
+    "Investigation Accession":"Investigation Identifier",
+    "Investigation Title":"Investigation Title",
+    "Public Release Date":"Investigation Public Release Date",
+    "PubMed ID":"Investigation PubMed ID",
+    "Publication DOI":"Investigation Publication DOI",
+    "Publication Author List":"Investigation Publication Author List",
+    "Publication Title":"Investigation Publication Title",
+    "Publication Status":"Investigation Publication Status",
+    "Publication Status Term Accession Number":"Investigation Publication Status Term Accession Number",
+    "Publication Status Term Source REF":"Investigation Publication Status Term Source REF",
+    "Person Last Name":"Investigation Person Last Name",
+    "Person First Name":"Investigation Person First Name",
+    "Person Mid Initials":"Investigation Person Mid Initials",
+    "Person Email":"Investigation Person Email",
+    "Person Phone":"Investigation Person Phone",
+    "Person Fax":"Investigation Person Fax",
+    "Person Address":"Investigation Person Address",
+    "Person Affiliation":"Investigation Person Affiliation",
+    "Person Roles":"Investigation Person Roles",
+    "Person Roles Term Accession Number":"Investigation Person Roles Term Accession Number",
+    "Person Roles Term Source REF":"Investigation Person Roles Term Source REF",
+    "Investigation Accession Term Source REF":"Comment[Investigation Accession Term Source REF]",
+    "MAGE-TAB Version":"Comment[MAGE-TAB Version]",
+    "Experiment Description":"Investigation Description"
+    }
+
+idf_to_isa_study = {
+    "Investigation Title":"Study Title",
+    "Experiment Description":"Study Description",
+    "Public Release Date":"Study Public Release Date",
+    "Experimental Design":"Study Design Type",
+    "Experimental Design Term Accession Number":"Study Design Type Term Accession Number",
+    "Experimental Design Term Source REF":"Study Design Type Term Source REF",
+    "PubMed ID":"Study PubMed ID",
+    "Publication DOI":"Study Publication DOI",
+    "Publication Author List":"Study Publication Author List",
+    "Publication Title":"Study Publication Title",
+    "Publication Status":"Study Publication Status",
+    "Publication Status Term Accession Number":"Study Publication Status Term Accession Number",
+    "Publication Status Term Source REF":"Study Publication Status Term Source REF",
+    "Experimental Factor Name":"Study Factor Name",
+    "Experimental Factor Type":"Study Factor Type",
+    "Experimental Factor Term Accession Number":"Study Factor Type Term Accession Number",
+    "Experimental Factor Term Source REF":"Study Factor Type Term Source REF",
+    "Protocol Hardware":"Study Assay Technology Platform",
+    "Protocol Name":"Study Protocol Name",
+    "Protocol Type":"Study Protocol Type",
+    "Protocol Term Accession Number":"Study Protocol Type Term Accession Number",
+    "Protocol Term Source REF":"Study Protocol Type Term Source REF",
+    "Protocol Description":"Study Protocol Description",
+    "Protocol Parameters":"Study Protocol Parameters Name",
+    "Person Last Name":"Study Person Last Name",
+    "Person First Name":"Study Person First Name",
+    "Person Mid Initials":"Study Person Mid Initials",
+    "Person Email":"Study Person Email",
+    "Person Phone":"Study Person Phone",
+    "Person Fax":"Study Person Fax",
+    "Person Address":"Study Person Address",
+    "Person Affiliation":"Study Person Affiliation",
+    "Person Roles":"Study Person Roles",
+    "Person Roles Term Accession Number":"Study Person Roles Term Accession Number",
+    "Person Roles Term Source REF":"Study Person Roles Term Source REF",
+    "Quality Control Type":"Comment[Quality Control Type]",
+    "Quality Control Type Term Accession Number":"Comment[Quality Control Type Term Accession Number]",
+    "Quality Control Type Term Source REF":"Comment[Quality Control Type Term Source REF]",
+    "Replicate Type":"Comment[Replicate Type]",
+    "Replicate Type Term Accession Number":"Comment[Replicate Type Term Accession Number]",
+    "Replicate Type Term Source REF":"Comment[Replicate Type Term Source REF]",
+    "Normalization Type":"Comment[Normalization Type]",
+    "Normalization Type Term Accession Number":"Comment[Normalization Term Accession Number]",
+    "Normalization Type Term Source REF":"Comment[Normalization Term Source REF]",
+    "Date of Experiment":"Comment[Date of Experiment]",
+    "Protocol Contact":"Comment[Protocol Contact]",
+    "Protocol Software":"Comment[Protocol Software]",
+    "SDRF File":"Comment[SDRF File]"
+    }
+
+idf_to_isa_assay = {
+    "Protocol Hardware":"Technology Platform",
+    "Quality Control Type":"Comment[Quality Control Type]",
+    "Quality Control Type Term Accession Number":"Comment[Quality Control Type Term Accession Number]",
+    "Quality Control Type Term Source REF":"Comment[Quality Control Type Term Source REF]",
+    "Replicate Type":"Comment[Replicate Type]",
+    "Replicate Type Term Accession Number":"Comment[Replicate Type Term Accession Number]",
+    "Replicate Type Term Source REF":"Comment[Replicate Type Term Source REF]",
+    "Normalization Type":"Comment[Normalization Type]",
+    "Normalization Type Term Accession Number":"Comment[Normalization Term Accession Number]",
+    "Normalization Type Term Source REF":"Comment[Normalization Term Source REF]",
+    "Date of Experiment":"Comment[Date of Experiment]",
+    "Protocol Software":"Comment[Protocol Software]",
+    "SDRF File":"Comment[SDRF File]"
+    }
+
+
+
+
+
+
+
+
+
+
+
+
