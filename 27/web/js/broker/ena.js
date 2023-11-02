5d8981fb797118e43d65ce57dcb702ec3d344129
Pedram Keyvani <pke003@uib.no>
2023-11-02 17:29:40 +0100

    ena broker

--- 27/web/js/broker/ena.js
+++ 27/web/js/broker/ena.js
@@ -1,55 +1,4 @@
-import { ena } from "./config.json";
-
-class EnaActions {
-  static Add = new EnaActions("ADD");
-  static Modify = new EnaActions("MODIFY");
-  static Validate = new EnaActions("VALIDATE");
-  static Cancel = new EnaActions("CANCEL");
-  static Receipt = new EnaActions("RECEIPT");
-  static Hold = new EnaActions("HOLD");
-  static Release = new EnaActions("RELEASE");
-
-  constructor(name) {
-    this.name = name;
-  }
-
-  get list() {
-    return [
-      EnaActions.Add,
-      EnaActions.Modify,
-      EnaActions.Validate,
-      EnaActions.call,
-      EnaActions.Cancel,
-      EnaActions.Receipt,
-      EnaActions.Hold,
-      EnaActions.Release,
-    ];
-  }
-}
-
-class EnaElements {
-  static Study = new EnaElements("STUDY");
-  static Project = new EnaElements("PROJECT");
-  static Sample = new EnaElements("SAMPLE");
-  static Experiment = new EnaElements("EXPERIMENT");
-  static Run = new EnaElements("RUN");
-  static Analysis = new EnaElements("ANALYSIS");
-
-  constructor(name) {
-    this.name = name;
-  }
-
-  get list() {
-    return [
-      EnaElements.Study,
-      EnaElements.Project,
-      EnaElements.Sample,
-      EnaElements.Experiment,
-      EnaElements.Run,
-      EnaElements.Analysis,
-    ];
-  }
-}
+import { ena as Config } from "./config.json";
 
 export class EnaBroker {
   constructor(isaJson) {
@@ -57,57 +6,14 @@ export class EnaBroker {
   }
 
   async submit() {
-    return fetch(ena.url, {
-      body: { action: EnaActions.Add, isaJson: this.isaJson },
-      headers: {},
-    });
-  }
-
-  parseReciept(xmlReciept) {
-    function getAccession(element) {
-      element.getAttribute("accession");
-    }
-
-    let responseJson = {
-      targetRepository: "ena",
-      receipt: xmlReciept,
-      accessions: [],
-    };
-
-    const parser = new DOMParser();
-    const reciept = parser
-      .parseFromString(xmlReciept)
-      .getElementsByTagName("RECEIPT")
-      .item(0);
-    const success =
-      String(reciept.getAttribute("success")).toLowerCase() === "true";
-    if (!success) throw "Submission is failed.";
-
-    for (const element of reciept.children) {
-      let accession = {
-        path: [],
-        value: getAccession(element),
-      };
-      switch (element.nodeName) {
-        case EnaElements.Study.name:
-          accession.path = [];
-          break;
-        case EnaElements.Project.name:
-          accession.path = [];
-          break;
-        case EnaElements.Sample.name:
-          accession.path = [];
-          break;
-        case EnaElements.Experiment.name:
-          accession.path = [];
-          break;
-        case EnaElements.Analysis.name:
-          accession.path = [];
-          break;
+    return fetch(
+      `${Config.url}?webinUserName=${Config.username}&webinPassword=${Config.password}`,
+      {
+        method: "POST",
+        body: this.isaJson,
       }
-      responseJson.accessions.push(accession);
-    }
+    );
   }
 
-  updateIsaJson() {}
+  mapToPathIsaJson(reciept) {}
 }
