ca5d88e651d6690cbefbc34d6cae03835cad55ab
Cameron Hyde <chyde@neoformit.com>
2023-11-02 17:41:37 +1000

    19 - fetch real yaml spec

--- 19/ena_template_builder/app/schema.py
+++ 19/ena_template_builder/app/schema.py
@@ -4,12 +4,14 @@ import yaml
 import requests
 
 SCHEMA_URL = (
-    'https://raw.githubusercontent.com/'
-    'neoformit/ena-upload-template-builder/main/data/example_schema.yml'
+    # 'https://raw.githubusercontent.com/'
+    # 'neoformit/ena-upload-template-builder/main/data/example_schema.yml'
+    'https://raw.githubusercontent.com/ELIXIR-Belgium/ENA-metadata-templates/'
+    'main/templates/{identifier}/{identifier}.yml'
 )
 
 
-def fetch():
+def fetch(identifier):
     """Fetch and parse schema definition from YAML."""
-    response = requests.get(SCHEMA_URL)
+    response = requests.get(SCHEMA_URL.format(identifier=identifier))
     return yaml.safe_load(response.text)
