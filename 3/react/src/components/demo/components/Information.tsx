485537bf4534e336a01029211e278ccbdf38e846
TomDijkema <tom.dijkema@naturalis.nl>
2023-11-03 09:26:46 +0100

    Add Docker image and Kubernetes files
    Commit adds Docker image and Kuberneters files.
    Also includes description of the project

--- 3/react/src/components/demo/components/Information.tsx
+++ 3/react/src/components/demo/components/Information.tsx
@@ -7,11 +7,29 @@ const Information = () => {
         <Row className="h-100">
             <Col className="h-100">
                 <div className="bgc-secondary px-4 py-3 h-100">
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-                        dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    <p className="fs-4">
+                        We are proud to present our project, "Building a Virtual Reference Collection for Bumblebees (Bombus),"
+                        developed during the BioHackathon 2023 of Elixir Europe. This project, represented as "Project 3: Automating
+                        the building of a virtual, distributed pollinator reference collection," aims to develop a linked open data
+                        schema that enhances the accessibility, organization, and utilization of taxonomic information for pollinators,
+                        particularly focusing on Bumblebees.
+                    </p>
+                    <p className="fs-4">
+                        We have developed a virtual reference collection to address the challenge of insufficient data for European bee species,
+                        potentially revealing more at-risk species and aiding naturalists and researchers. Our project aims to expand species
+                        identification tools to improve pollinator observations, fostering large-scale collaboration and citizen science for the
+                        identification and preservation of these crucial species.
+                    </p>
+                    <p className="fs-4">
+                        Our demo schema, using the JSON data-interchange format and linked to databases like GBIF and Fauna Europeana, serves as an
+                        essential prototype for the broader "TETTRIs" project, aligning the virtual pollinator reference collection with existing data
+                        standards like Darwin Core etc.
+                    </p>
+                    <p className="fs-4">
+                        The future prospects of our virtual reference collection schema involve expanding it with additional data elements, transitioning to a
+                        linked data-based approach, making it open access on platforms like GitHub, establishing connections to databases like BOLD, ENA,
+                        and GloBI to facilitate informed identifications, and contributing to scientific knowledge and community engagement, ultimately serving
+                        as a vital resource for pollinator conservation and biodiversity research.
                     </p>
                 </div>
             </Col>
